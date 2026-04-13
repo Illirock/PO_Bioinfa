c443e4ff32bb32a67a5000f341f5787afe8206f9
Patryk Mendelewski <90185123+Illirock@users.noreply.github.com>
2026-04-13 17:25:30 +0200

    Update Lab5.cpp

--- LAB05_13_04_26/Lab5.cpp
+++ LAB05_13_04_26/Lab5.cpp
@@ -5,11 +5,11 @@ using namespace std;
 const int SIZE = 5;
 
 template <typename Type>
-class MaxArray {
+class Array {
 private:
     Type arr[SIZE];
 public:
-    MaxArray(Type input[SIZE]) {
+    Array(Type input[SIZE]) {
         for (int i = 0; i < SIZE; i++) {
             arr[i] = input[i];
         }
@@ -33,9 +33,9 @@ int main() {
     float float_array[SIZE] = { 6.7, 4.7, 2.1, 3.7, 6.9 };
     char char_array[SIZE] = { 'M', 'u', 'R', 'a', 'D' };
 
-    MaxArray<int> int_object(int_array);
-    MaxArray<float> float_object(float_array);
-    MaxArray<char> char_object(char_array);
+    Array<int> int_object(int_array);
+    Array<float> float_object(float_array);
+    Array<char> char_object(char_array);
 
     cout << "Max int: " << int_object.findMax() << endl;
     cout << "Max float: " << float_object.findMax() << endl;
