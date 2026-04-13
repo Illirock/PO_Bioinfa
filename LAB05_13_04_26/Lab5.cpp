4b1f2cf1cc5d8d65354dc3a8b87b4d5c56415283
Patryk Mendelewski <90185123+Illirock@users.noreply.github.com>
2026-04-13 17:09:03 +0200

    Add files via upload

---
+++ LAB05_13_04_26/Lab5.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+
+using namespace std;
+
+const int SIZE = 5;
+
+template <typename Type>
+class MaxArray {
+private:
+    Type arr[SIZE];
+public:
+    MaxArray(Type input[SIZE]) {
+        for (int i = 0; i < SIZE; i++) {
+            arr[i] = input[i];
+        }
+    }
+
+    Type findMax() {
+        Type max = arr[0];
+
+        for (int i = 1; i < SIZE; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+        return max;
+    }
+};
+
+
+int main() {
+    int int_array[SIZE] = { 6, 7, 4, 2, 0 };
+    float float_array[SIZE] = { 6.7, 4.7, 2.1, 3.7, 6.9 };
+    char char_array[SIZE] = { 'M', 'u', 'R', 'a', 'D' };
+
+    MaxArray<int> int_object(int_array);
+    MaxArray<float> float_object(float_array);
+    MaxArray<char> char_object(char_array);
+
+    cout << "Max int: " << int_object.findMax() << endl;
+    cout << "Max float: " << float_object.findMax() << endl;
+    cout << "Max char: " << char_object.findMax() << endl;
+
+    return 0;
+}
